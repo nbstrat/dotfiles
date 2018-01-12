be4f5654bcc772a8b1d818a589f2d0519fe9b580
Shane Walters <michael.walters.18.ctr@us.af.mil>
2018-01-12 14:36:25 -0600

    replaced full path to worklog output file with $HOME, added worklog to gitignore

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -45,7 +45,7 @@ alias gitcheats='vim /c/Users/1287921639E/Documents/CheatSheets_Reference/Git/gi
 # other stuff
 alias vs='"/c/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/devenv.exe"'
 alias cmd='"%windir%\system32\cmd.exe"'
-alias worklog='vim /c/Users/1287921639E/worklog.txt'
+alias worklog='vim $HOME/worklog.txt'
 alias dash='clear &&  ls -l && echo && git branch && echo && git status --short --branch'
 #alias dn='git status --short --branch | grep '^.[DM\?]' | head -1 | awk '$1 ~ /[MD]/ {print $2} $1 ~ /\?/ {print "/dev/null " $2}' | xargs git diff -- && w'
 #alias an='git status --short --branch | grep '^.[DM\?]' | head -1 | awk '$1 ~ /[M?]/ {print "add " $2} $1 ~ /D/ {print "rm " $2}' | xargs git && w'
