b45882f5aa9f75d7309c840c9457d447ef7e60a8
Shane Walters <nbstrat@gmail.com>
2019-03-06 14:08:55 -0600

    GetHash test from 700 server to development report server. Memory exception with just this simple test case. PUNT!

--- .bash/GetHash.ps1
+++ .bash/GetHash.ps1
@@ -13,10 +13,10 @@ The site location that script will be execute at. Expected values are KAFB or LA
 add any helpful notes here
 
 .EXAMPLE
+From a Remote Desktop Connection on the server
 Open Power Shell
 cd to the script directory
-(I am running it from my .bash directory)
-cd $home/.bash 
+cd E:\DELIVERY\scm\CM_Scripts\Release 
 
 .\GetHash.ps1 KAFB
 .\GetHash.ps1 LAFB
@@ -36,8 +36,13 @@ param (
 # Set output width to max integer to prevent ellipsis output
 $OUTPUTWIDTH = 2147483647
 
-$LOGDIR = "C:\Users\1287921639E\.bash\hash-output\"
-$RELEASE = "xx_xx_xx"
+#TODO: Directories must exist prior to outputing the log file.
+#$RELEASE = "xx_xx_xx"
+$RELEASE = "hash-test"
+$LOGDIR = "E:\DELIVERY\Release\hash-test\Output_Files\Directory_Logs\"
+
+#$LOGDIR = "C:\Users\1287921639E\.bash\hash-output\"
+#$RELEASE = "xx_xx_xx"
 $OUTPUTPATH = $LOGDIR+$RELEASE
 $OUTPUTFILE = ""
 
@@ -67,23 +72,25 @@ if ($SITE -eq "KAFB") {
 
    # Report Server
    # TODO: Move script to 700 server and update Path to report server location
+   #TODO: Need a try-catch block in the event the server cannot be connected to.
+   # TODO: Output of memory exception when attempting to get hash from inetpub directory.
     Write-Host "Reports Server Inetpub"
    $OUTPUTFILE = $OUTPUTPATH + '_Report_Server_MAGH-MP-706v.log'
-   Get-ChildItem -Path c:\_projects\custom_reports\_deliverables\* -Recurse `
+   Get-ChildItem -Path \\mahg-mp-792v\c$\inetpub\wwwroot\ttmsreportsapp\* -Recurse `
     -Exclude *.xml,*.tmp,*.txt,*.pdf | 
      Sort-Object Directory, Name |
      Select-Object @{n='RootDirectory     ';ex={$_.Directory.name}},Name,CreationTime,@{n='SHA256';ex={(Get-FileHash -Algorithm SHA256 $_.fullname).hash}} | 
      Out-File -Width $OUTPUTWIDTH -FilePath $OUTPUTFILE
-   $OUTPUTFILE = $OUTPUTPATH + '_Report_Server_MAGH-MP-706v_Config.log'
-   Get-ChildItem -Path c:\_projects\custom_reports\`{tomcat_home`}\conf\* -Recurse |
-     Sort-Object Directory, Name |
-     Select-Object @{n='RootDirectory     ';ex={$_.Directory.name}},Name,CreationTime,@{n='SHA256';ex={(Get-FileHash -Algorithm SHA256 $_.fullname).hash}} | 
-     Out-File -Width $OUTPUTWIDTH -FilePath $OUTPUTFILE
-   $OUTPUTFILE = $OUTPUTPATH + '_Report_Server_MAGH-MP-706v_webapps.log'
-   Get-ChildItem -Path c:\_projects\custom_reports\`{tomcat_home`}\webapps\* -Recurse |
-     Sort-Object Directory, Name |
-     Select-Object @{n='RootDirectory     ';ex={$_.Directory.name}},Name,CreationTime,@{n='SHA256';ex={(Get-FileHash -Algorithm SHA256 $_.fullname).hash}} | 
-     Out-File -Width $OUTPUTWIDTH -FilePath $OUTPUTFILE 
+  #  $OUTPUTFILE = $OUTPUTPATH + '_Report_Server_MAGH-MP-706v_Config.log'
+  #  Get-ChildItem -Path c:\_projects\custom_reports\`{tomcat_home`}\conf\* -Recurse |
+  #    Sort-Object Directory, Name |
+  #    Select-Object @{n='RootDirectory     ';ex={$_.Directory.name}},Name,CreationTime,@{n='SHA256';ex={(Get-FileHash -Algorithm SHA256 $_.fullname).hash}} | 
+  #    Out-File -Width $OUTPUTWIDTH -FilePath $OUTPUTFILE
+  #  $OUTPUTFILE = $OUTPUTPATH + '_Report_Server_MAGH-MP-706v_webapps.log'
+  #  Get-ChildItem -Path c:\_projects\custom_reports\`{tomcat_home`}\webapps\* -Recurse |
+  #    Sort-Object Directory, Name |
+  #    Select-Object @{n='RootDirectory     ';ex={$_.Directory.name}},Name,CreationTime,@{n='SHA256';ex={(Get-FileHash -Algorithm SHA256 $_.fullname).hash}} | 
+  #    Out-File -Width $OUTPUTWIDTH -FilePath $OUTPUTFILE 
 
 
 # TODO: Add Cognos Datamart Server
