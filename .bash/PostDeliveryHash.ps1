c96f9e6d29ee6dd7a4daa789e06eed3ca7671e51
Shane Walters <nbstrat@gmail.com>
2019-03-12 10:40:14 -0500

    Add BufferWidth to obtain the wide format needed for the output file.

--- .bash/PostDeliveryHash.ps1
+++ .bash/PostDeliveryHash.ps1
@@ -49,6 +49,7 @@ param (
  [string] $SITE
 )
 
+[console]::BufferWidth = 4096
 
 #TODO: Remove archives\16_xx_xx from PATHDIR 
 $PATHDIR = "E:\DELIVERY\Release\archives\16_xx_xx\"
@@ -145,13 +146,20 @@ foreach ($server in $serverList) {
 # to add this formatting, change the format option below to Format-Table $tableFormat
 # using the Wrap parameter will output all the data but with wrapping
 $tableFormat = @{Expression={$_.Name}; Label="Name"; Width=28},
-                @{Expression={$_.LastWriteTime}; Label="LastWriteTime"; Width=24},
-                @{Expression={$_.SHA256}; Label="SHA256"; Width=67},
-                @{Expression={$_.HostServer}; Label="HostServer"; Width=20}
+                @{Expression={$_.SHA256}; Label="SHA256f"; Width=75},
+                @{Expression={$_.HostServer}; Label="HostServer"; Width=20},
+                @{Expression={$_.LastWriteTime}; Label="LastWriteTime"; Width=23}
 
 # output the results sorting my file name.
 # TODO Per the docs, Format-Table does not return any object and should be used last when piping outputs.
- $hashResults | ForEach {[PSCustomObject]$_} |
-    Sort-Object -Property Name |
-    Format-Table -Wrap |
-    Out-File -FilePath $OUTPUTFILE 
+  $hashResults | ForEach {[PSCustomObject]$_} |
+     Sort-Object -Property Name |
+     Format-Table -AutoSize |
+     Out-File -FilePath $OUTPUTFILE 
+
+#  $hashResults | ForEach {[PSCustomObject]$_} |
+#     Sort-Object -Property Name |
+#     Format-Table $tableFormat |
+#     Out-File -FilePath $OUTPUTFILE 
+    
+     
